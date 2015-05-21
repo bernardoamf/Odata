ccd42d74577b8a4c8db01a4e3f364d18ebc13d37
unknown <bernardm@Bernardm3.redmond.corp.microsoft.com>
2015-05-21 13:11:42 -0700

    Initial Solution

---
+++ WebAPI/WebAPI/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    class Customer
+    {
+        public int ID;
+        public string firstName;
+        public string lastName;
+        public string country;
+        public string companyName;
+        public bool active;
+    }
+}
---
+++ WebAPI/WebAPI/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    class Orders
+    {
+        public int ID;
+        public int customerID;
+        public DateTime date;
+        public string status;
+        public int salesPersonID;
+    
+    }
+}
